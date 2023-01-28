6620ecc907272a4893509cef7def213f4435a44a
belgrano9 <36034688+belgrano9@users.noreply.github.com>
2023-01-28 14:47:05 +0100

    Add files via upload
    Initial upload

---
+++ densities_1.cpp
@@ -0,0 +1,43 @@
+#include <random>
+#include <iostream>
+#include <cmath>
+#include "densities_1.h"
+
+// Generate random numbers from a pareto distribution
+std::default_random_engine generator;
+std::uniform_real_distribution<double> distribution(0.0, 1.0);
+std::normal_distribution<double> standard_normal(0.0, 1.0);
+
+dist::dist(double param1_, double param2_, DisType TheDisType_):
+    param1(param1_),
+    param2(param2_),
+    TheDisType(TheDisType_)
+{
+}
+
+double dist::compute()
+{
+    switch (TheDisType)
+    {
+        case normal:{
+            double z = standard_normal(generator);
+            return param1 + param2*z;}
+        case pareto:{
+            double u = distribution(generator);
+            double x = param1 / pow(u, 1.0 / param2);
+            return x;}
+        case student:{
+            std::chi_squared_distribution<double> chi_squared(param1);
+            double v = standard_normal(generator);
+            double t = v / sqrt(chi_squared(generator) / param1);
+            return t;}
+        default:    
+            throw std::runtime_error("Unknown distribution type found.");
+    }
+    return 0;
+}
+
+
+
+
+
